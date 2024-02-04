8ccb6d405aade5971c7b98a6842dbe58db5edfb8
Tomas Hejlik <tomas.hejlik@plus4u.net>
2024-02-04 21:39:35 +0100

    Add project files.

---
+++ ChessLogic/Moves/DoublePawn.cs
@@ -0,0 +1,29 @@
+﻿namespace ChessLogic
+{
+    public class DoublePawn : Move
+    {
+        public override MoveType Type => MoveType.DoublePawn;
+
+        public override Position FromPos { get; }
+
+        public override Position ToPos { get; }
+
+        private readonly Position skippedPosition;
+
+        public DoublePawn(Position from, Position to)
+        {
+            FromPos = from;
+            ToPos = to;
+            skippedPosition = new Position(( from.Row + to.Row ) / 2, from.Column);
+        }
+
+        public override bool Execute(Board board)
+        {
+            Player player = board[FromPos].Color;
+            board.SetPawnSkipPosition(player, skippedPosition);
+            new NormalMove(FromPos, ToPos).Execute(board);
+
+            return true;
+        }
+    }
+}
